d0cedc7ecaa31133486d1efa92a17273fb08022f
LeonKuehnle <104006250+LeonKuehnle@users.noreply.github.com>
2023-11-09 10:25:28 +0100

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,23 +1,20 @@
-import sqlite3
-
 def login(username, password):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
-    # Verwenden Sie benutzereingabe direkt in der SQL-Abfrage, was eine Sicherheitslücke darstellt
     query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
     cursor.execute(query)
 
     user = cursor.fetchone()
 
     if user:
-        print("Erfolgreich eingeloggt!")
+        return "Erfolgreich eingeloggt!"
     else:
-        print("Fehler beim Einloggen!")
-
-    conn.close()
+        return "Fehler beim Einloggen!"
 
 if __name__ == "__main__":
-    username = input("Benutzername: ")
-    password = input("Passwort: ")
-    login(username, password)
+    # Simulieren von Benutzereingaben mit Dummy-Daten
+    username = "testuser"
+    password = "testpassword"
+    result = login(username, password)
+    print(result)
